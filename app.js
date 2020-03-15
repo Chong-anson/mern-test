c056338c0e2163d3215d7584f357c7978b31ae3e
anson <ansonchongwus44@gmail.com>
2020-03-15 13:25:07 -0700

    test

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const bodyParser = require("body-parser");
 const passport = require('passport');
 const path = require('path');
 
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 mongoose
     .connect(db, { 
         useNewUrlParser: true, 
@@ -21,6 +29,7 @@ mongoose
 app.use(bodyParser.urlencoded({
     extended: false
 })); 
+
 
 app.use(bodyParser.json());
 app.use(passport.initialize());
@@ -48,10 +57,3 @@ app.listen(port, () => {
 });
 // take in callback 
 // run callback when it start listenting 
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
